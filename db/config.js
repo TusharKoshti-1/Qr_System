19cde4d1090367cd995cb63a96d07fda1be47450
TusharKoshti-1 <tusharkoshti01@gmail.com>
2025-02-17 22:57:37 +0530

    config

--- db/config.js
+++ db/config.js
@@ -1,20 +1,22 @@
-const mysql = require('mysql2');
-require('dotenv').config();  // This will load the .env variables
+const mysql = require("mysql2");
+require("dotenv").config();
+// This will load the .env variables
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST, // Hostname provided by InfinityFree
-  user: process.env.DB_USER,            // MySQL Username
-  password: process.env.DB_PASS,          // MySQL Password
+  user: process.env.DB_USER, // MySQL Username
+  password: process.env.DB_PASS, // MySQL Password
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,    // MySQL Database Name (replace XXX with your actual database name)
+  port: process.env.DB_PORT, // MySQL Database Name (replace XXX with your actual database name)
 });
+
 
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.stack);
+    console.error("Error connecting to the database:", err.stack);
     return;
   }
-  console.log('Connected to the database');
+  console.log("Connected to the database");
 });
 
 module.exports = connection;
